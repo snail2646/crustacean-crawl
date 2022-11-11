ac8c43c5af96a8b8cf25150b0b0782e7b549db32
Max Starr <max@maxstarr.xyz>
2022-11-11 11:31:47 -0600

    main boilerplate and prelude

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+mod map;
+
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+}
+
+use prelude::*;
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, World!");
+    }
+}
+
+impl State {
+    fn new() -> Self {
+        Self {}
+    }
+}
+fn main() -> BError {
+    let context = BTermBuilder::simple80x50()
+        .with_title("Crustacean Crawl")
+        .build()?;
+    main_loop(context, State::new())
 }
