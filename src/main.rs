389e31de13814e2053e0a86c2925bc4c5c211f7e
Max Starr <max@maxstarr.xyz>
2022-11-17 11:05:27 -0600

    add display constants

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ mod map;
 mod map_builder;
 mod player;
 mod prelude {
-    pub use bracket_lib::prelude::*;
-    pub const SCREEN_WIDTH: i32 = 80;
-    pub const SCREEN_HEIGHT: i32 = 50;
     pub use crate::map::*;
     pub use crate::map_builder::*;
     pub use crate::player::*;
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
+    pub const DISPLAY_HEIGHT: i32 = SCREEN_HEIGHT / 2;
 }
 
 use prelude::*;
