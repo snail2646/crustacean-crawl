1b6972b99708fa91a2fe4c90e82e4a8791811bdd
Max Starr <max@maxstarr.xyz>
2022-12-01 11:19:55 -0600

    initial add legion

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ mod prelude {
     pub use crate::map_builder::*;
     pub use crate::player::*;
     pub use bracket_lib::prelude::*;
+    pub use legion::systems::CommandBuffer;
+    pub use legion::world::SubWorld;
+    pub use legion::*;
     pub const SCREEN_WIDTH: i32 = 80;
     pub const SCREEN_HEIGHT: i32 = 50;
     pub const DISPLAY_WIDTH: i32 = SCREEN_WIDTH / 2;
