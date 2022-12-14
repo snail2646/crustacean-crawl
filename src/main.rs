0c48391075e5070736c1b014be656e43ee276efa
Max Starr <max@maxstarr.xyz>
2022-12-13 22:33:58 -0600

    map render system

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,7 @@ impl GameState for State {
 
         self.resources.insert(ctx.key);
         self.systems.execute(&mut self.ecs, &mut self.resources);
+        render_draw_buffer(ctx).expect("Render error");
     }
 }
 
